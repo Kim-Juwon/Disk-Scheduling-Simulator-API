1378b8ad78dbaa577d626327351b9259ed39c964
김주원 <damiano102777@naver.com>
2023-06-15 13:08:28 +0900

    First commit

---
+++ src/main/java/link/diskscheduler/DiskSchedulingSimulatorApiApplication.java
@@ -0,0 +1,13 @@
+package link.diskscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiskSchedulingSimulatorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiskSchedulingSimulatorApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/link/diskscheduler/DiskSchedulingSimulatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package link.diskscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiskSchedulingSimulatorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
