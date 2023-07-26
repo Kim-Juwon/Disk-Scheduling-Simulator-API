dc03660c8967f9bc6e57063a03e8ffdcaf9df7c8
김주원 <damiano102777@naver.com>
2023-07-26 11:21:17 +0900

    refactor: cylinder number 상수 수정

--- src/main/java/link/diskscheduler/constant/ProgramConstant.java
+++ src/main/java/link/diskscheduler/constant/ProgramConstant.java
@@ -3,8 +3,8 @@ package link.diskscheduler.constant;
 public class ProgramConstant {
     public static final long MIN_CYLINDER_COUNT = 1L;
     public static final long MAX_CYLINDER_COUNT = 1000L;
-    public static final long MIN_CYLINDER_NUMBER = MIN_CYLINDER_COUNT - 1;
-    public static final long MAX_CYLINDER_NUMBER = MAX_CYLINDER_COUNT - 1;
+    public static final long MIN_CYLINDER_NUMBER = 0L;
+    public static final long MAX_CYLINDER_NUMBER = MAX_CYLINDER_COUNT - 1L;
     public static final int MIN_REQUESTED_CYLINDER_COUNT = 1;
     public static final int MAX_REQUESTED_CYLINDER_COUNT = 1000;
     public static final long MIN_ARRIVAL_TIME = 0L;
